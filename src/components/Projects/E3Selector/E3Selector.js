c1126f636c98c27c1545ec490bcc61fe5c886111
charludo <github@charlotteharludo.com>
2021-05-31 16:25:20 +0200

    added sample Mui components to get familiar with the system

--- src/components/Projects/E3Selector/E3Selector.js
+++ src/components/Projects/E3Selector/E3Selector.js
@@ -1,5 +1,9 @@
 import React, {Component} from 'react';
 import Button from '@material-ui/core/Button';
+import Checkbox from '@material-ui/core/Checkbox';
+import Slider from '@material-ui/core/Slider';
+import Fab from '@material-ui/core/Fab';
+import TextField from '@material-ui/core/TextField';
 import { createMuiTheme, StylesProvider, ThemeProvider } from '@material-ui/core/styles'
 import CssBaseline from "@material-ui/core/CssBaseline"
 import "./App.sass"
@@ -42,8 +46,21 @@ class E3Selector extends Component {
                 <StylesProvider>
                     <ThemeProvider theme={theme}>
                         <CssBaseline />
+                        <h1>E3 Selector</h1>
                         <Button variant="contained" color="primary">Primary</Button>
-                        <Button variant="contained" color="secondary">Secondary</Button>
+                        <br/><br/><Button variant="contained" color="secondary">Secondary</Button>
+                        <br/><br/><Checkbox color="primary" value="checkedA" inputProps={{ 'aria-label': 'Checkbox A' }}/>
+                        <br/><br/><Fab variant="extended" color="primary">Search</Fab>
+                        <br/><br/><TextField
+                          required
+                          id="outlined-required"
+                          label="Required"
+                          variant="outlined"
+                        />
+                        <Slider
+                          defaultValue={3}
+                          width="20%"
+                        />
                     </ThemeProvider>
                 </StylesProvider>
             </div>
