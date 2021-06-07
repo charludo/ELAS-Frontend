1d2851918805bd20492559543105e8c3a7b29a48
charludo <github@charlotteharludo.com>
2021-06-07 16:01:37 +0200

    redid changes

--- src/components/Projects/E3Selector/Filters.jsx
+++ src/components/Projects/E3Selector/Filters.jsx
@@ -14,63 +14,63 @@ export default function Filters(props) {
 
       <FormControlLabel
           value="end"
-          control={<Checkbox defaultChecked={true} onClick={() => props.action("locales", "Essen")} color="primary"/>}
+          control={<Checkbox defaultChecked={true} onClick={() => {props.action("locales", "Essen"); props.action("locales", "Essen (UKE)");}} color="primary"/>}
           label="Essen"
           labelPlacement="end"
         />
 
 <br/><br/><FormControlLabel
           value="end"
-          control={<Checkbox defaultChecked={true} onClick={() => props.action("locales", "Duisburg")} color="primary" />}
+          control={<Checkbox defaultChecked={true} onClick={() => {props.action("locales", "Duisburg"); props.action("locales", "Duisburg (B)"); props.action("locales", "Duisburg (L/M)");}} color="primary" />}
           label="Duisburg"
           labelPlacement="end"
         />
 
 <br/><br/><FormControlLabel
           value="end"
-          control={<Checkbox defaultChecked={true} color="primary" />}
+          control={<Checkbox defaultChecked={true} color="primary" onClick={() => props.action("locales", "Bochum")}/>}
           label="Bochum"
           labelPlacement="end"
           />
 
  <FormControlLabel
           value="end"
-          control={<Checkbox defaultChecked={true} color="primary" />}
+          control={<Checkbox defaultChecked={true} color="primary" onClick={() => props.action("locales", "Dortmund")}/>}
           label="Dortmund"
           labelPlacement="end"
         />
 
 <FormControlLabel
           value="end"
-          control={<Checkbox defaultChecked={true} color="primary" />}
+          control={<Checkbox defaultChecked={true} color="primary" onClick={() => {props.action("exam", "Schriftliche Ausarbeitung");props.action("exam", "Klausur")}}/>}
           label="Written"
           labelPlacement="end"
         />
 
 <FormControlLabel
           value="end"
-          control={<Checkbox defaultChecked={true} color="primary" />}
+          control={<Checkbox defaultChecked={true} color="primary" onClick={() => props.action("exam", "Mündliche Prüfung")}/>}
           label="Oral"
           labelPlacement="end"
         />
 
 <FormControlLabel
           value="end"
-          control={<Checkbox defaultChecked={true} color="primary" />}
+          control={<Checkbox defaultChecked={true} color="primary" onClick={() => props.action("exam", "Essay")}/>}
           label="Essay"
           labelPlacement="end"
         />
 
 <FormControlLabel
           value="end"
-          control={<Checkbox defaultChecked={true} color="primary" />}
+          control={<Checkbox defaultChecked={true} color="primary" onClick={() => props.action("exam", "Präsentation")}/>}
           label="Presentation"
           labelPlacement="end"
         />
 
 <FormControlLabel
           value="end"
-          control={<Checkbox defaultChecked={true} color="primary" />}
+          control={<Checkbox defaultChecked={true} color="primary" onClick={() => props.action("languages", "Deutsch")}/>}
           label="German"
           labelPlacement="end"
         />
@@ -78,21 +78,21 @@ export default function Filters(props) {
 
         <FormControlLabel
           value="end"
-          control={<Checkbox defaultChecked={true} color="primary" />}
+          control={<Checkbox defaultChecked={true} color="primary" onClick={() => props.action("languages", "Englisch")}/>}
           label="English"
           labelPlacement="end"
         />
 
 <FormControlLabel
           value="end"
-          control={<Checkbox defaultChecked={true} color="primary" />}
+          control={<Checkbox defaultChecked={true} color="primary" onClick={() => props.action("languages", "Türkisch")}/>}
           label="Turkish"
           labelPlacement="end"
         />
 
 <FormControlLabel
           value="end"
-          control={<Checkbox defaultChecked={true} color="primary" />}
+          control={<Checkbox defaultChecked={true} color="primary" onClick={() => props.action("languages", "Niederländisch")}/>}
           label="Dutch"
           labelPlacement="end"
         />
@@ -100,35 +100,35 @@ export default function Filters(props) {
 
 <FormControlLabel
           value="end"
-          control={<Checkbox defaultChecked={true} color="primary" />}
+          control={<Checkbox defaultChecked={true} color="primary" onClick={() => props.action("courseType", "VL/Übung")}/>}
           label="Lecture + Exercise"
           labelPlacement="end"
         />
 
 <FormControlLabel
           value="end"
-          control={<Checkbox defaultChecked={true} color="primary" />}
+          control={<Checkbox defaultChecked={true} color="primary" onClick={() => props.action("courseType", "Vorlesung")}/>}
           label="Lecture"
           labelPlacement="end"
         />
 
 <FormControlLabel
           value="end"
-          control={<Checkbox defaultChecked={true} color="primary" />}
+          control={<Checkbox defaultChecked={true} color="primary" onClick={() => props.action("courseType", "Seminar")}/>}
           label="Seminar"
           labelPlacement="end"
         />
 
 <FormControlLabel
           value="end"
-          control={<Checkbox defaultChecked={true} color="primary" />}
+          control={<Checkbox defaultChecked={true} color="primary" onClick={() => props.action("courseType", "Blockseminar")}/>}
           label="Blocked Seminar"
           labelPlacement="end"
         />
 
 <FormControlLabel
           value="end"
-          control={<Checkbox defaultChecked={true} color="primary" />}
+          control={<Checkbox defaultChecked={true} color="primary" onClick={() => props.action("courseType", "E-Learning")}/>}
           label="E-Learning"
           labelPlacement="end"
         />
