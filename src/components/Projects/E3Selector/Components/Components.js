61e4b20ed405e56f3308ba13c0d24e1ae9a5bc0d
charludo <github@charlotteharludo.com>
2021-06-14 10:34:16 +0200

    created a factory function for checkboxes

--- src/components/Projects/E3Selector/Components/Components.js
+++ src/components/Projects/E3Selector/Components/Components.js
@@ -1,15 +1,35 @@
 import React, {Component} from 'react';
 import Checkbox from '@material-ui/core/Checkbox';
+import {Grid} from '@material-ui/core';
 import "./Components.sass";
 
-const CheckBox = (props) => (
-	<button onClick={() => props.action()}>{props.text}</button>
-);
+const CheckBox = (props) => {
+	const actionIterator = (action, params) => {
+		params.forEach(p => {
+			action(p[0], p[1]);
+		});
+	};
+
+	return (
+		<Checkbox defaultChecked={props.checked} color={props.color} classes={props.classes} onClick={() => {actionIterator(props.action, props.arguments)}}/>
+	);
+}
+
+CheckBox.defaultProps = {
+	action: null,
+	arguments: [],
+	checked: true,
+	color: "primary",
+	classes: ""
+}
 
 export { CheckBox };
 
 const TimeTable = (props) => (
-	<Checkbox label="Test" defaultChecked={true} color="primary"/>
+	<div>
+		<CheckBox classes="abc" action={alert} arguments={["a", "b"]}/>
+		<h1>Divider</h1>
+	</div>
 );
 
 export default TimeTable;
